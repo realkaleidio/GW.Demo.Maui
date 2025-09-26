b3b7d9a52c8fa2ef1a08455b8711dc30c5a294ed
realkaleidio <realkaleidio@gmail.com>
2025-09-26 11:31:26 +0200

    0.0.3 (5.1.1.3) Funktion in Side Menu auswählen

--- GW.Demo.Maui/AppShell.xaml.cs
+++ GW.Demo.Maui/AppShell.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace GW.Demo.Maui
+﻿namespace GW.Demo.Maui;
+
+using System.Windows.Input;
+
+
+
+public partial class AppShell: Shell
 {
-    public partial class AppShell : Shell
+    public ICommand SelectCommand { get; private init; }
+
+
+    public AppShell()
     {
-        public AppShell()
+        SelectCommand = new Command<string>(async s =>
         {
-            InitializeComponent();
-        }
+            int idx = -1;
+            if(!int.TryParse(s, out idx)) { idx = -1; }
+
+            if (Shell.Current.CurrentPage is Views.SwipeHostPage p)
+            {
+                p.NavigateTo(idx);
+            }
+            else { await Shell.Current.GoToAsync($"//swipe?index={idx}"); }
+
+            Shell.Current.FlyoutIsPresented = false;
+        });
+
+        InitializeComponent();
+        BindingContext = this;
     }
 }
